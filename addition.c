b25798113e086c1aecc774d7d60e2496bcd52dc2
VishvajitJamdade01 <vishvajitjamdade25@gmail.com>
2026-04-23 12:29:24 +0530

    commit

---
+++ addition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    printf("Enter the first number : ");
+    scanf("%d",&a);
+    printf("Enter the second number : ");
+    scanf("%d",&b);
+    int c = a + b;
+    printf("The addition of %d and %d is %d",a,b,c);
+};
