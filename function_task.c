fec8dc832bc8f7ac9c5586be078b593ecd0fc853
VishvajitJamdade01 <vishvajitjamdade25@gmail.com>
2026-04-27 10:50:26 +0530

    function

---
+++ function_task.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int add(int x,int y){
+    int z = x+y;
+    return z;
+}
+
+int sub(int x,int y){
+    int z = x-y;
+    return z;
+}
+
+int mult(int x,int y){
+    int z = x*y;
+    return z;
+}
+
+int divi(int x,int y){
+    int z = x/y;
+    return z;
+}
+
+int main(){
+    printf("Addition = %d\n",add(20,10));
+    printf("Substraction = %d\n",sub(20,10));
+    printf("Multiplication = %d\n",mult(20,10));
+    printf("Division = %d\n",divi(20,10));
+}
