595ecb9624cee6be5d20b8782995ac5a4aa0baec
VishvajitJamdade01 <vishvajitjamdade25@gmail.com>
2026-04-27 10:44:31 +0530

    function

--- function.c
+++ function.c
@@ -1,10 +1,30 @@
 #include<stdio.h>
 
 int hello(){
-    printf("Hello Function");
+    printf("Hello Function\n");
     return 0;
+}
+
+
+//types of function
+// default function
+int add(){
+    int a = 10;
+    int b = 45;
+    int c = a+b;
+    printf("%d\n",c);
+    return c;
+}
+
+//parameterized function
+int substract(int x, int y){  //parameters
+    int c = x+y;
+    printf("%d\n",c);
+    return c;
 }
 
 int main(){
     hello();
+    add();
+    substract(10,100); //arguments
 }
